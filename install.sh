74f9c9ee7bd49a3b41f7dadc61976ad75c86a480
Luke Underwood <moresilenter@gmail.com>
2017-04-10 22:56:52 -0700

    Add in YouCompleteMe installer

--- install.sh
+++ install.sh
@@ -2,10 +2,13 @@
 # Installs the dotfiles onto a newly provisioned server
 #
 # We use GNU stow to place the dotfiles, so here we install that
-if [[ $(which apt-get) ]]; then
-  which stow || sudo apt-get update && sudo apt-get install -y stow
-elif [[ $(which yum) ]]; then
-  which stow || yum install -y stow --enablerepo=epel
+
+# Command check notation borrowed from:
+# http://stackoverflow.com/a/677212/1243823
+if [[ $(hash apt-get 2>/dev/null) ]]; then
+  hash stow 2>/dev/null || sudo apt-get update && sudo apt-get install -y stow
+elif [[ $(hash yum 2>/dev/null) ]]; then
+  hash stow 2>/dev/null || yum install -y stow --enablerepo=epel
 else
   echo "Neither apt-get nor yum found. Terminating..."
   exit 1
@@ -14,3 +17,14 @@ fi
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 stow -t "$HOME"/ -S "$DIR"/root
+
+# Now fix YouCompleteMe
+if [[ ! $(hash python 2>/dev/null) && $(hash git 2>/dev/null) ]]; then
+  echo "Python or git is missing. Skipping optional YouCompleteMe install."
+  exit 0
+fi
+
+pushd $DIR/root/.vim/bundle/YouCompleteMe
+git submodule update --init --recursive
+python install.py --tern-completer
+popd
