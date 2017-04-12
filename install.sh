9f04f85a4d052647f7401454f5513af7c9bfdb4f
Luke Underwood <moresilenter@gmail.com>
2017-04-11 20:44:08 -0700

    Update install and remove YouCompleteMe

--- install.sh
+++ install.sh
@@ -5,26 +5,33 @@
 
 # Command check notation borrowed from:
 # http://stackoverflow.com/a/677212/1243823
-if [[ $(hash apt-get 2>/dev/null) ]]; then
-  hash stow 2>/dev/null || sudo apt-get update && sudo apt-get install -y stow
-elif [[ $(hash yum 2>/dev/null) ]]; then
-  hash stow 2>/dev/null || yum install -y stow --enablerepo=epel
-else
-  echo "Neither apt-get nor yum found. Terminating..."
-  exit 1
+if ! hash stow 2>/dev/null; then
+  if hash apt-get 2>/dev/null; then
+    hash stow 2>/dev/null || sudo apt-get update && sudo apt-get install -y stow
+  elif hash yum 2>/dev/null; then
+    hash stow 2>/dev/null || yum install -y stow --enablerepo=epel
+  else
+    echo "Neither apt-get nor yum found. Terminating..."
+    exit 1
+  fi
 fi
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-stow -t "$HOME"/ -S "$DIR"/root
+echo "stow -t $HOME/ -S $DIR/root"
+pushd $DIR
+stow -t "$HOME"/ -S root
+popd
 
 # Now fix YouCompleteMe
-if [[ ! $(hash python 2>/dev/null) && $(hash git 2>/dev/null) ]]; then
+if ! hash python 2>/dev/null && hash git 2>/dev/null; then
   echo "Python or git is missing. Skipping optional YouCompleteMe install."
   exit 0
 fi
 
-pushd $DIR/root/.vim/bundle/YouCompleteMe
+pushd $DIR/root/.vim/bundle
 git submodule update --init --recursive
+pushd YouCompleteMe
 python install.py --tern-completer
+popd
 popd
