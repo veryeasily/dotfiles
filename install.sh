9d5b7f70bf5aaf7158f0e60e814c0f06c9bce69c
Luke Underwood <moresilenter@gmail.com>
2017-04-12 20:36:34 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,11 +5,15 @@
 
 # Command check notation borrowed from:
 # http://stackoverflow.com/a/677212/1243823
+if hash apt-get 2>/dev/null; then
+  apt-get update
+fi
+
 if ! hash stow 2>/dev/null; then
   if hash apt-get 2>/dev/null; then
-    hash stow 2>/dev/null || sudo apt-get update && sudo apt-get install -y stow
+    hash stow 2>/dev/null || sudo apt-get install -y stow
   elif hash yum 2>/dev/null; then
-    hash stow 2>/dev/null || yum install -y stow --enablerepo=epel
+    hash stow 2>/dev/null || sudo yum install -y stow --enablerepo=epel
   else
     echo "Neither apt-get nor yum found. Terminating..."
     exit 1
@@ -24,10 +28,12 @@ stow -t "$HOME"/ -S root
 popd
 
 if ! hash zsh 2>/dev/null; then
-  chsh -s $(which zsh)
+  sudo apt install -y zsh
 fi
 
-# Now fix YouCompleteMe
+chsh -s $(which zsh)
+
+Now fix YouCompleteMe
 if ! hash python 2>/dev/null && hash git 2>/dev/null; then
   echo "Python or git is missing. Skipping optional YouCompleteMe install."
   exit 0
