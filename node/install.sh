7ab350a91e7cfa929f568fa9f49a31f8e9379a8d
Luke Underwood <moresilenter@gmail.com>
2017-10-10 01:56:28 -0700

    Fix gitmodules

--- node/install.sh
+++ node/install.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env zsh
 
-echo "inside the node install"
-
 # if ! hash yarn 2>/dev/null; then
 #   curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 #   echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
--- ruby/install.sh
+++ ruby/install.sh
@@ -1,11 +1,13 @@
 # rbenv should already be setup but we need to add the plugins and install a
 # global ruby
 
-RBENV_DIR="$( dirname "$0:A" )"
+CUR_SCRIPT_DIR="$( dirname "$0:A" )"
 
 if ! hash rbenv 2>/dev/null; then
   eval "$("$HOME"/.rbenv/bin/rbenv init -)"
 fi
+
+RBENV_DIR="$(rbenv root)"
 
 # Install ruby-build
 # https://github.com/rbenv/ruby-build
@@ -14,7 +16,7 @@ git clone https://github.com/rbenv/ruby-build.git "$(rbenv root)"/plugins/ruby-b
 # Install default-gems
 # https://github.com/rbenv/rbenv-default-gems
 git clone https://github.com/rbenv/rbenv-default-gems.git "$(rbenv root)"/plugins/rbenv-default-gems 2>/dev/null
-ln -s "$RBENV_DIR"/default-gems "$(rbenv root)/default-gems"
+[[ -h "$RBENV_DIR"/default-gems ]] || ln -s "$CUR_SCRIPT_DIR"/default-gems "$RBENV_DIR"/default-gems
 
 RB_VERSION="$(rbenv install -l | grep -P -o '(?<=\s|^)(?:\d+\.){2}\d+(?=\s|$)' | tail -n 1)"
 rbenv install "$RB_VERSION"
