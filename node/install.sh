005d136268a9c8677a5c7a88273132a3431a7539
Luke Underwood <moresilenter@gmail.com>
2017-10-13 00:42:40 -0700

    More nvmrc

--- node/install.sh
+++ node/install.sh
@@ -2,15 +2,20 @@
 
 SCRIPT_DIR="$(dirname "$0:A")"
 
-if [[ ! -d "$HOME"/.nvm ]]; then
+if [[ ! -e "$HOME"/.nvm ]]; then
   curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.5/install.sh | bash
   export NVM_DIR="$HOME/.nvm"
   [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
   nvm install node
-  nvm alias default node
 fi
 
+[[ -e "$HOME"/.nvmrc ]] || {
+  echo 'Copying .nvmrc to HOME directory'
+  cp "$SCRIPT_DIR"/.nvmrc "$HOME"/.nvmrc
+}
+
 echo "$SCRIPT_DIR"
+
 if ! hash yarn 2>/dev/null; then
   . "$SCRIPT_DIR"/install-yarn.sh
 fi
