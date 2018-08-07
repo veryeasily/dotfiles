d0a14caef836772cf6059321660c2094f73f6241
Luke Underwood <veryveryeasily@gmail.com>
2018-08-07 00:45:21 -0700

    Add in SSH key warning

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/zsh
+local ssh_files="$(find "$HOME"/.ssh -type f)"
+if ! echo "$ssh_files" | egrep '(id_dsa|id_ecdsa|id_ed25519|id_rsa).pub'; then
+  echo "No SSH keys detected!"
+  echo "Make sure you've mounted your ssh keys at /root/.ssh so zplug can work"
+  exit 1
+fi
 source ~/.zplug/init.zsh
 source ~/.zsh/main.zsh
 zplug install
